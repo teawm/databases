68672611cbf347b852229a78355c02d9750bf71b
teawm <165138961+teawm@users.noreply.github.com>
2026-09-13 21:56:01 +0700

    replace lab1.5.sql

--- lab1.5.sql
+++ lab1.5.sql
@@ -1,3 +1,3 @@
 INSERT INTO sal (snum, name, comm, city) VALUES 
-(3006, 'TechnoPark', 0.14, 'Kazan'),
-(3007, 'MegaTech', 0.10, 'Tula');
+(3006, 'Astra', 0.16, 'Innopolis'),
+(3007, 'RedSoft', 0.13, 'Moscow');
